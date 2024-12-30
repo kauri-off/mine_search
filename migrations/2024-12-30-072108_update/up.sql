8e4fd19e77731998ddb93ff9d74f1fe70201f025
Kauri <kauri.off@gmail.com>
2024-12-30 11:00:41 +0300

    db migration complete

--- migrations/2024-12-30-072108_update/up.sql
+++ migrations/2024-12-30-072108_update/up.sql
@@ -1,24 +1,5 @@
 -- Your SQL goes here
 
-CREATE TABLE server_new (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    ip TEXT NOT NULL UNIQUE, -- Переименованное поле с ограничением UNIQUE
-    online INTEGER NOT NULL,
-    max INTEGER NOT NULL,
-    version_name TEXT NOT NULL,
-    protocol INTEGER NOT NULL,
-    license BOOLEAN NOT NULL,
-    white_list BOOLEAN
-);
+ALTER TABLE server RENAME COLUMN addr TO ip;
 
-PRAGMA foreign_keys = OFF;
-
-INSERT INTO server_new (id, ip, online, max, version_name, protocol, license, white_list)
-SELECT id, addr, online, max, version_name, protocol, license, white_list
-FROM server;
-
-DROP TABLE server;
-
-ALTER TABLE server_new RENAME TO server;
-
-PRAGMA foreign_keys = ON;
+CREATE UNIQUE INDEX IF NOT EXISTS ux_ip ON server(ip);
