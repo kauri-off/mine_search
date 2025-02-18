eb8095054cc42b5ed83516f55f5b1300fb03fb02
Kauri <kauri.off@gmail.com>
2025-02-18 21:54:42 +0300

    api fix

--- frontend/src/api/apiConfig.ts
+++ frontend/src/api/apiConfig.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const baseURL: string = import.meta.env.VITE_BASE_URL ?? "http://localhost:3000/api";
-
 const apiClient = axios.create({
-  baseURL: baseURL,
+  baseURL: '/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
