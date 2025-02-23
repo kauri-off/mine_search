bbd9ef3185596fa8a20896e7990cc3bb0d89922f
Kauri <kauri.off@gmail.com>
2025-02-23 13:31:19 +0300

    fix: added limit | new filter

--- backend/src/api/fetch_server_list.rs
+++ backend/src/api/fetch_server_list.rs
@@ -44,28 +44,21 @@ pub async fn fetch_server_list(
             .unwrap_or_default()
     };
 
-    let server_list = match body.license {
-        Some(l) => {
-            servers::table
-                .left_join(players::table.on(players::server_id.eq(servers::id.nullable())))
-                .filter(servers::id.lt(server_id))
-                .filter(servers::license.eq(l))
-                .group_by(servers::id)
-                .select((ServerModel::as_select(), count(players::id).nullable()))
-                .load::<(ServerModel, Option<i64>)>(&mut db.pool.get().await.unwrap())
-                .await
-        }
-        None => {
-            servers::table
-                .left_join(players::table.on(players::server_id.eq(servers::id.nullable())))
-                .filter(servers::id.lt(server_id))
-                .group_by(servers::id)
-                .select((ServerModel::as_select(), count(players::id).nullable()))
-                .load::<(ServerModel, Option<i64>)>(&mut db.pool.get().await.unwrap())
-                .await
-        }
-    }
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let filter: Box<dyn BoxableExpression<_, diesel::pg::Pg, SqlType = diesel::sql_types::Bool>> =
+        match body.license {
+            Some(t) => Box::new(servers::id.lt(server_id).and(servers::license.eq(t))),
+            None => Box::new(servers::id.lt(server_id)),
+        };
+
+    let server_list = servers::table
+        .left_join(players::table.on(players::server_id.eq(servers::id.nullable())))
+        .filter(filter)
+        .limit(body.limit)
+        .group_by(servers::id)
+        .select((ServerModel::as_select(), count(players::id).nullable()))
+        .load::<(ServerModel, Option<i64>)>(&mut db.pool.get().await.unwrap())
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(Json(
         server_list
